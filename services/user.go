0fec8f6b442f4ac57859143f3224c2b47940278d
Jefferson Santos <jefferson.pereira@ifood.com.br>
2022-08-21 02:38:16 -0300

    implementing bidirectional stream in server

--- services/user.go
+++ services/user.go
@@ -15,7 +15,7 @@ import (
 //	mustEmbedUnimplementedUserServiceServer()
 //  AddUserVerbose(ctx context.Context, in *User, opts ...grpc.CallOption) (UserService_AddUserVerboseClient, error)
 // 	AddUsers(ctx context.Context, opts ...grpc.CallOption) (UserService_AddUsersClient, error)
-
+//  AddUserStreamBoth(ctx context.Context, opts ...grpc.CallOption) (UserService_AddUserStreamBothClient, error)
 //}
 
 type UserService struct {
@@ -86,5 +86,26 @@ func (*UserService) AddUsers(stream pb.UserService_AddUsersServer) error {
 		})
 
 		fmt.Println("Adding", request.GetName())
+	}
+}
+
+func (*UserService) AddUserStreamBoth(stream pb.UserService_AddUserStreamBothServer) error {
+
+	for {
+		request, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			log.Fatalf("Error receiving stream from client %v", err)
+		}
+
+		err = stream.Send(&pb.UserResultStream{
+			Status: "Added",
+			User:   request,
+		})
+		if err != nil {
+			log.Fatalf("Error sending stream to the client %v", err)
+		}
 	}
 }
